196fab1b102e2e21dbe254d6134b710e50747100
Vaibu269 <100691072+Vaibu269@users.noreply.github.com>
2024-12-01 07:52:59 +0000

    varibles

---
+++ practise/variable.js
@@ -0,0 +1,29 @@
+// const myName="vaibhav"
+// let userName="abc"
+// var password="1234"
+// location="sangola"
+
+// console.table([myName,userName,password,location])
+
+// // myName="vishal"
+// userName="xyz"
+// password="2312"
+// location="hatid"
+// console.table([userName,password,location])
+// console.log(typeof(location));
+// console.log(typeof(myName));
+
+let myNumber = 10;
+let myString = String(myNumber);
+console.log(myString);
+
+let name="vaibhav";
+let number=Number(name);
+console.log(number);
+console.log(typeof(number));
+
+let num=1;
+let isCommited=Boolean(num);
+console.log(isCommited);
+
+
